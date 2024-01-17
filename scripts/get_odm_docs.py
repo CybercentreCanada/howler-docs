ea12260b0e102701f77fd4e5ae35ad13beb36bb2
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2024-01-17 12:58:17 -0500

    More work on documentation

--- scripts/get_odm_docs.py
+++ scripts/get_odm_docs.py
@@ -2,37 +2,46 @@ from pathlib import Path
 import shlex
 import subprocess
 
-import yaml
-
 root_dir = Path(__file__).parent.parent
 parent_dir = Path(__file__).parent.parent.parent
 
-howler_dir = parent_dir / "howler-api"
+print("Generating Howler API documentation")
+
+howler_api_dir = parent_dir / "howler-api"
 if not (parent_dir / "howler-api").exists():
-    howler_dir = Path(input("Enter complete path to howler-api:\n> "))
+    howler_api_dir = Path(input("Enter complete path to howler-api:\n> "))
 
-print(f"Executing from {howler_dir}")
+print(f"\tExecuting from {howler_api_dir}")
 
-venv_dir = next(howler_dir.glob("*env"))
+venv_dir = next(howler_api_dir.glob("*env"))
 
 output = subprocess.check_output(
-    shlex.split(f"{venv_dir}/bin/python {howler_dir}/build_scripts/generate_md_docs.py")
+    shlex.split(
+        f"{venv_dir}/bin/python {howler_api_dir}/build_scripts/generate_md_docs.py"
+    )
 )
 
+yaml_output_preface = ["- Data Ontology:"]
 yaml_output = []
+handled_first = False
 for md in output.decode().strip().split("\n\n\n\n"):
-    class_name = md.split("\n")[0]
-
-    relative_path = (
-        root_dir.relative_to(parent_dir) / "docs/odm/class" / (class_name + ".md")
-    )
-    print(f"Writing to {relative_path}")
+    file_name = md.split("\n")[0]
+    relative_path = root_dir.relative_to(parent_dir) / file_name
+    print(f"\t\tWriting to {relative_path}")
 
     (parent_dir / relative_path).parent.mkdir(parents=True, exist_ok=True)
     (parent_dir / relative_path).write_text("\n".join(md.split("\n")[1:]))
-    yaml_output.append(f"    - {relative_path.relative_to('howler-docs/docs')}")
+    if not handled_first:
+        yaml_output_preface.append(
+            f"  - Getting Started: {relative_path.relative_to('howler-docs/docs')}"
+        )
+        yaml_output_preface.append("  - Classes:")
+        handled_first = True
+    else:
+        yaml_output.append(f"    - {relative_path.relative_to('howler-docs/docs')}")
 
-print('\nAdd the following below "Data Ontology" in mkdocs.yml:')
+print("\n\tAdd the following below nav in mkdocs.yml:")
 
-print("  - Classes:")
+print()
+print("\n".join(yaml_output_preface))
 print("\n".join(sorted(yaml_output)))
